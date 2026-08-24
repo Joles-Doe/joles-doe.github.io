e79e07aedb55cab0bd49ec9c4a704048c96a4c67
Joles-Doe <105786625+Joles-Doe@users.noreply.github.com>
2026-08-24 21:25:58 +0100

    Routing fix

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 
+import { Introduction } from '../introduction/introduction';
+
 @Component({
-  imports: [],
+  imports: [
+    Introduction
+  ],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
